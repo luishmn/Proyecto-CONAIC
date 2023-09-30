0a2c64b2dc9a162739802fad2e608a389f035d65
oscar23w <63673374+oscar23w@users.noreply.github.com>
2023-09-30 11:16:57 -0600

    Agregue formulario de registro en ver usuario

--- visualizacion_usuario/index_visual_usu.php
+++ visualizacion_usuario/index_visual_usu.php
@@ -40,6 +40,109 @@
             </div>
         </form>
     </section>
+
+    
+
+    <!-- ESTE ES EL FORMULARIO PARA REGISTRAR UN NUEVO USUARIO -->
+    <div id="formularioContainer" class="oculto">
+
+            <!-- Contenido de tu formulario aquí -->
+            <form class="from-login" action="registrarUsuarioBD.php" method="post">
+
+                <h1 class="centrar">Registrar Usuarios</h1>
+                <br>
+
+                <div class="contenedor">
+                    <div class="form_c1">
+                        <div class="form_group">
+                            <input type="text" id="nombre" class="form_input" placeholder=" " name="nombre" required>
+                            <label for="nombre" class="form_label">Nombre:</label>
+                        </div>
+                    </div>
+                    <div class="form_c2">
+                        <div class="form_group">
+                            <input type="text" id="apellidoP" class="form_input" placeholder=" " name="apellidoP"
+                                required>
+                            <label for="apellidoP" class="form_label">Apellido Paterno:</label>
+                        </div>
+                    </div>
+                    <div class="form_c3">
+                        <div class="form_group">
+                            <input type="text" id="apellidoM" class="form_input" placeholder=" " name="apellidoM"
+                                required>
+                            <label for="apellidoM" class="form_label">Apellido Materno:</label>
+                        </div>
+                    </div>
+                </div>
+
+                <br><br><br><br>
+
+                <div class="contenedor">
+                    <div class="form_c4">
+                        <div class="form_group">
+                            <input type="text" id="cargo" class="form_input" placeholder=" " name="cargo" required>
+                            <label for="cargo" class="form_label">Cargo Desempeñado:</label>
+                        </div>
+                    </div>
+
+                    <div class="form_c9">
+                        <select name="tipoUsuario" id="tipoUsuario" class="form_input" required>
+                            <option value="normal">Usuario Normal</option>
+                            <option value="admin">Usuario Administrador</option>
+                        </select>
+
+                    </div>
+                </div>
+
+                <br><br><br><br>
+
+                <div class="form_c5">
+                    <div class="form_group">
+                        <input type="email" id="correo" class="form_input" placeholder=" " name="correo" required>
+                        <label for="correo" class="form_label">Correo Electronico:</label>
+                    </div>
+                </div>
+
+                <br><br><br><br>
+
+                <div class="contenedor">
+                    <div class="form_c6">
+                        <div class="form_group">
+                            <input type="password" id="contrasena" class="form_input" placeholder=" " name="contrasena"
+                                required>
+                            <label for="contrasena" class="form_label">Contraseña:</label>
+                        </div>
+                    </div>
+
+                    <div class="form_c7">
+                        <div class="form_group">
+                            <input type="password" id="contrasenaV" class="form_input" placeholder=" "
+                                name="contrasenaV" required>
+                            <label for="contrasenaV" class="form_label">Confirmar Contraseña:</label>
+                        </div>
+                    </div>
+
+                    <div class="form_c8">
+                        <button id="verpassword" disabled><img src="../imagenes/verPassword.png" alt=""
+                                id="ojo"></button>
+                        <script src="../registrar_usuario/verpassword.js"></script>
+                    </div>
+                </div>
+
+                <br><br><br><br>
+
+                <div class="form_c10">
+                    <button type="submit" id="registrar">Registar Usuario</button>
+                </div>
+
+            
+        </form>
+    </div>
+
+    <div id="fondoOscuro" class="oculto"></div>
+
+
+
     <br><br><br>
 
     <?php
@@ -108,10 +211,26 @@
     <img src="../imagenes/logo_Fondo.png" id="imgLogoFondo">
     
     </div>
+    
+    
+
 </body>
 
 </html>
 
+<script> // Script para aparecer y desaparecer el formulario de registro
+    document.getElementById("Registrar").addEventListener("click", function() {
+    // Mostrar el fondo oscuro y el formulario
+    document.getElementById("fondoOscuro").style.display = "block";
+    document.getElementById("formularioContainer").style.display = "block";
+});
+
+document.getElementById("fondoOscuro").addEventListener("click", function() {
+    // Ocultar el fondo oscuro y el formulario cuando se hace clic fuera del formulario
+    document.getElementById("fondoOscuro").style.display = "none";
+    document.getElementById("formularioContainer").style.display = "none";
+});
+</script>
 
 <script>
 
@@ -137,13 +256,13 @@
 
 
      // Selecciona el botón por su ID
-     var botonGuardar = document.getElementById("Registrar");
+     // var botonGuardar = document.getElementById("Registrar");
 
     // Agrega un manejador de eventos al botón
-    botonGuardar.addEventListener("click", function() {
+    // botonGuardar.addEventListener("click", function() {
         // Redirecciona al archivo "nuevo.html"
-        window.location.href = "../registrar_usuario/registraUsuarios.html";
-    });
+       // window.location.href = "../registrar_usuario/registraUsuarios.html";
+    //});
 
 
 
