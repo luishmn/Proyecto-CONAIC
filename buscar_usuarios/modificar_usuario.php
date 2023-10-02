6e5060a30b973830a5e3c63665fd9983835ea925
Diego Fregoso <imdiegofregoso@gmail.com>
2023-10-02 14:19:38 -0600

    Buscar usuarios en tiempo real

---
+++ buscar_usuarios/modificar_usuario.php
@@ -0,0 +1,6 @@
+<?php
+$userId = $_GET['userId'];
+
+echo ('Este es el id del usuario a modificar: '.$userId);
+echo ('<br>');
+echo ('Por lo tanto aqui va el codigo para modificar o eliminar el usuario');
