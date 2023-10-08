46c3123272ff9aab9114368de6a933766834be28
oscar23w <63673374+oscar23w@users.noreply.github.com>
2023-10-08 11:24:10 -0600

    arreglado error de las instrucciones de contraseña

--- Eliminar Usuario y asignaciones de subcriterios del usuario/eliminar_usuario.php
+++ Eliminar Usuario y asignaciones de subcriterios del usuario/eliminar_usuario.php
@@ -16,15 +16,20 @@ $correo = $conn->real_escape_string($correo);
 $sql_check = "SELECT correo FROM usuario WHERE correo = '$correo'"; 
 $result_check = $conn->query($sql_check);
 
+
+
 if ($result_check->num_rows == 0) {
     echo "El usuario con el correo $correo no existe en la base de datos.";
 
 
 } else {
     $sql_delete = "DELETE FROM asignacionsubcriterio WHERE correo = '$correo'"; 
-    $sql_delete = "DELETE FROM usuario WHERE correo = '$correo'"; 
+    
     if ($conn->query($sql_delete) === TRUE) {
-        echo "Usuario con el correo $correo eliminado exitosamente.";
+        $sql_delete = "DELETE FROM usuario WHERE correo = '$correo'"; 
+        if ($conn->query($sql_delete) === TRUE) {
+            echo "Usuario con el correo $correo eliminado exitosamente.";
+        }
     } else {
         echo "Error al eliminar al usuario: " . $conn->error;
     }
