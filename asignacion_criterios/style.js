1705dbe43f8cb98cffe2d6250f5f5898c1503836
Madnessl3 <phentaxalex@gmail.com>
2023-10-22 13:08:14 -0600

    barra de busqueda de usuarios p asignacion

--- asignacion_criterios/style.js
+++ asignacion_criterios/style.js
@@ -11,22 +11,46 @@ details.forEach((detail) => {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    var selectedOption = document.querySelector('.selected-option');
-    var optionsList = document.querySelector('.options');
-    var selectedValue = document.querySelector('.usuario_seleccionado');
-  
-    selectedOption.addEventListener('click', function() {
-      optionsList.style.display = (optionsList.style.display === 'block') ? 'none' : 'block';
+    var input = document.getElementById('busqueda');
+    var lista = document.getElementById('options');
+    var usuarios = lista.getElementsByTagName('li');
+
+    function filtrarlista() {
+        var filtro = input.value.toLowerCase();
+        
+        for (var i=0; i<usuarios.length; i++) {
+            var texto = usuarios[i].innerText.toLowerCase();
+            if (texto.includes(filtro)) {
+                usuarios[i].style.display = 'block';
+            } else {
+                usuarios[i].style.display = 'none';
+            }
+        }
+    }
+
+    input.addEventListener('focus', function() {
+        lista.style.display = 'block';
     });
-  
-    optionsList.addEventListener('click', function(event) {
-      if (event.target.tagName === 'LI') {
-        selectedOption.textContent = "Selecciona una opción";
-        selectedValue.textContent = event.target.textContent;
-        optionsList.style.display = 'none';
-      }
+
+    input.addEventListener('input',function() {
+        filtrarlista();
     });
-  });
+
+    for (var i=0; i<usuarios.length; i++) {
+        usuarios[i].addEventListener('click', function(){
+            input.value = this.innerText;
+            lista.style.display = 'none';
+        });
+    }
+
+    document.addEventListener('click', function(event) {
+        if (event.target != input && event.target != lista) {
+            lista.style.display = 'none';
+        }
+    });
+
+});
+
 
 
 
@@ -184,3 +208,5 @@ document.querySelector(".options").addEventListener("click", function(e) {
 });
 
 });
+
+
