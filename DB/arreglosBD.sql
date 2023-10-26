9e77e71cb1a0d50adc1084b93731382898253e4f
oscar23w <63673374+oscar23w@users.noreply.github.com>
2023-10-26 08:13:17 -0600

    ..

--- DB/arreglosBD.sql
+++ DB/arreglosBD.sql
@@ -1,1 +1,49 @@
+USE CONAIC
+
 UPDATE `subcriterio` SET `nombre` = 'Deben existir procedimientos que garanticen la calidad de los trabajos de titulación en el que participen las academias o algún grupo colegiado designado para tal fin y con participación externa.' WHERE `subcriterio`.`claveSubCriterio` = '2.5.3';
+
+INSERT INTO `subcriterio` (`nombre`, `claveSubCriterio`) VALUES ('Debe existir un procedimiento permanente de evaluación curricular.', '3.7.4');
+
+INSERT INTO `subcriterio` (`nombre`, `claveSubCriterio`) VALUES ('Deben existir mecanismos para la promoción externa (visitas a planteles de nivel medio superior,\r\ntrípticos, difusión en medios masivos de comunicación, etc.) del programa.', '3.8.3');
+
+INSERT INTO `criteriossubcriterio` (`claveCriterio`, `claveSubCriterio`) VALUES ('3.7', '3.7.4'), ('3.8', '3.8.3');
+
+DELETE FROM `criteriossubcriterio` WHERE `criteriossubcriterio`.`claveCriterio` = '3.6' AND `criteriossubcriterio`.`claveSubCriterio` = '3.6.2';
+DELETE FROM `criteriossubcriterio` WHERE `criteriossubcriterio`.`claveCriterio` = '3.6' AND `criteriossubcriterio`.`claveSubCriterio` = '3.6.3';
+DELETE FROM `criteriossubcriterio` WHERE `criteriossubcriterio`.`claveCriterio` = '3.6' AND `criteriossubcriterio`.`claveSubCriterio` = '3.6.4';
+
+DELETE FROM `subcriterio` WHERE `subcriterio`.`claveSubCriterio` = '3.6.2';
+DELETE FROM `subcriterio` WHERE `subcriterio`.`claveSubCriterio` = '3.6.3';
+DELETE FROM `subcriterio` WHERE `subcriterio`.`claveSubCriterio` = '3.6.4';
+
+UPDATE `criterio` SET `nombre` = 'Flexibilidad Curricular' WHERE `criterio`.`claveCriterio` = '3.6';
+UPDATE `criterio` SET `nombre` = 'Evaluación y actualización' WHERE `criterio`.`claveCriterio` = '3.7';
+UPDATE `criterio` SET `nombre` = 'Difusión' WHERE `criterio`.`claveCriterio` = '3.8';
+
+INSERT INTO `criterio` (`nombre`, `claveCriterio`) VALUES ('Justificación de las Competencias', '3.9');
+INSERT INTO `criterioscategoria` (`claveCriterio`, `claveCategoria`) VALUES ('3.9', '3');
+
+UPDATE `subcriterio` SET `nombre` = 'Indique las materias optativas ofrecidas en los últimos tres años.\r\nLas unidades de la asignatura y las áreas debe considerarse en términos de la clasificación indicada en las\r\nrespuestas 3.1 y 3.2' WHERE `subcriterio`.`claveSubCriterio` = '3.6.1';
+
+DELETE FROM `criteriossubcriterio` WHERE `criteriossubcriterio`.`claveCriterio` = '3.7' AND `criteriossubcriterio`.`claveSubCriterio` = '3.7.1';
+DELETE FROM `criteriossubcriterio` WHERE `criteriossubcriterio`.`claveCriterio` = '3.7' AND `criteriossubcriterio`.`claveSubCriterio` = '3.7.2';
+DELETE FROM `criteriossubcriterio` WHERE `criteriossubcriterio`.`claveCriterio` = '3.7' AND `criteriossubcriterio`.`claveSubCriterio` = '3.7.3';
+
+DELETE FROM `subcriterio` WHERE `subcriterio`.`claveSubCriterio` = '3.7.1';
+DELETE FROM `subcriterio` WHERE `subcriterio`.`claveSubCriterio` = '3.7.2';
+DELETE FROM `subcriterio` WHERE `subcriterio`.`claveSubCriterio` = '3.7.3';
+
+INSERT INTO `subcriterio` (`nombre`, `claveSubCriterio`) VALUES ('El plan de estudios debe ser revisado y actualizado en su caso, al menos cada cinco años', '3.7.1'), ('Debe existir un procedimiento oficial y funcional,  para la revisión y actualización del plan de\r\nestudios. \r\n', '3.7.2'), ('En los procesos de revisión y actualización deben participar los cuerpos colegiados, así como un grupo de asesores externos representantes del sector productivo, egresados en activo e  investigadores\r\nreconocidos', '3.7.3');
+INSERT INTO `criteriossubcriterio` (`claveCriterio`, `claveSubCriterio`) VALUES ('3.7', '3.7.1'), ('3.7', '3.7.2'), ('3.7', '3.7.3');
+
+DELETE FROM `criteriossubcriterio` WHERE `criteriossubcriterio`.`claveCriterio` = '3.8' AND `criteriossubcriterio`.`claveSubCriterio` = '3.8.1';
+DELETE FROM `criteriossubcriterio` WHERE `criteriossubcriterio`.`claveCriterio` = '3.8' AND `criteriossubcriterio`.`claveSubCriterio` = '3.8.2';
+
+DELETE FROM `subcriterio` WHERE `subcriterio`.`claveSubCriterio` = '3.8.1';
+DELETE FROM `subcriterio` WHERE `subcriterio`.`claveSubCriterio` = '3.8.2';
+
+INSERT INTO `subcriterio` (`nombre`, `claveSubCriterio`) VALUES ('¿Los programas actualizados de todas las asignaturas del plan de estudios están a disposición para\r\nsu consulta por parte de profesores, estudiantes y el público en general?', '3.8.1'), ('Indique cuáles de los siguientes aspectos se le da conocer al estudiante', '3.8.2');
+INSERT INTO `criteriossubcriterio` (`claveCriterio`, `claveSubCriterio`) VALUES ('3.8', '3.8.1'), ('3.8', '3.8.2');
+
+INSERT INTO `subcriterio` (`nombre`, `claveSubCriterio`) VALUES ('Tabla de cumplimiento de competencias transversales. Considerar la definición y justificación\r\ncompetencias iniciales, de desarrollo y de evaluación.  Rellenar tabla competencias transversales. Etapa\r\nde planificación del modelo de competencias.\r\n', '3.9.1'), ('Tabla de cumplimiento de competencias específicas. Considerar la definición y justificación\r\ncompetencias iniciales, de desarrollo y de evaluación. Rellenar tabla competencias específicas. Etapa de\r\nplanificación del modelo de competencias.\r\n', '3.9.2');
+INSERT INTO `criteriossubcriterio` (`claveCriterio`, `claveSubCriterio`) VALUES ('3.9', '3.9.1'), ('3.9', '3.9.2');
---
+++ categorias/anexos.php
@@ -0,0 +1,215 @@
+<?php
+    session_start();
+    //$_SESSION['loggedin']= false;
+    // Verifica si el usuario ha iniciado sesión
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+        // Accede al nombre de usuario almacenado en la sesión
+        $nombreUsuario = $_SESSION['username'];
+        $correoUsuario = $_SESSION['email'];
+        $tipo = $_SESSION['tipo'];
+    } else {
+        // Si no ha iniciado sesión, redirige al usuario a la página de inicio de sesión
+        header('Location: ../index.php');
+        exit;
+    }
+
+    $nombre = substr($nombreUsuario, 0, 10);
+    
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <script>var correoUsuario = "<?php echo $correoUsuario; ?>";</script>
+    <script>var tipoUsuario = "<?php echo $tipo; ?>";</script>
+
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anexos</title>
+    <link rel="stylesheet" href="autoevaluacion.css">
+    <script src="enviarConsulta.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+</head>
+
+<body>
+    
+
+    <header>
+        <div class="barra-superior"><!-- ESTE ES EL DIV DE LA BARRA SUPERIOR -->
+        <a href="" class="enlace-inicio" id="enviarInicio"><i class="fas fa-home"></i></a>
+
+     
+        
+            <label class="L1">Autoevaluación</label>
+
+            <button class="menu_estilo_usuario">
+                <img src="../PrincipalUsuario/usuario.png" alt="Usuario"> 
+                <div class="texto"> <?php echo $nombreUsuario; ?></div>
+            </button>
+
+            
+        </div>
+    </header>
+
+
+    <img src="logo_CONAIC_letras.png" class="logoArriba">
+
+    <div class="lablCat">
+                <label class="catE">Categorías</label><br>
+        </div>
+    <div class="todo">
+        
+        <div class="cuadro2">
+            <div>
+                <div class="elemetosMenuLateral" id="1">
+                    <hr>
+                    <a href="categoria1.php">
+                        <p class="parrafo1">Categoría 1 <i class="fas fa-arrow-right" ></i></p>
+                    </a> 
+                </div>
+                <div class="elemetosMenuLateral" id="2">
+                    <hr>
+                    <a href="categoria2.php">
+                        <p class="parrafo1">Categoría 2 <i class="fas fa-arrow-right" ></i></p>
+                    </a>
+                </div>
+                <div class="elemetosMenuLateral" id="3">
+                    <hr>
+                    <a href="categoria3.php">
+                        <p class="parrafo1">Categoría 3 <i class="fas fa-arrow-right" ></i></p>
+                    </a>
+                </div>
+                <div class="elemetosMenuLateral" id="4">
+                    <hr>
+                    <a href="categoria4.php">
+                        <p class="parrafo1">Categoría 4  <i class="fas fa-arrow-right" ></i></p>
+                    </a>
+                </div>
+                <div class="elemetosMenuLateral" id="5">
+                    <hr>
+                    <a href="categoria5.php">
+                        <p class="parrafo1">Categoría 5 <i class="fas fa-arrow-right" ></i></p>
+                    </a>
+                </div>
+                <div class="elemetosMenuLateral" id="6">
+                    <hr>
+                    <a href="categoria6.php">
+                        <p class="parrafo1">Categoría 6 <i class="fas fa-arrow-right" ></i></p>
+                    </a>
+                </div>
+                <div class="elemetosMenuLateral" id="7">
+                    <hr>
+                    <a href="categoria7.php">
+                        <p class="parrafo1">Categoría 7 <i class="fas fa-arrow-right" ></i></p>
+                    </a>
+                </div>
+                <div class="elemetosMenuLateral" id="8">
+                    <hr>
+                    <a href="categoria8.php">
+                        <p class="parrafo1">Categoría 8 <i class="fas fa-arrow-right" ></i></p>
+                    </a>
+                </div>
+                <div class="elemetosMenuLateral" id="9">
+                    <hr>
+                    <a href="categoria9.php">
+                        <p class="parrafo1">Categoría 9 <i class="fas fa-arrow-right" ></i></p>
+                    </a>
+                </div>
+                <div class="elemetosMenuLateral" id="10">
+                    <hr>
+                    <a href="categoria10.php">
+                        <p class="parrafo1">Categoría 10 <i class="fas fa-arrow-right" ></i></p>
+                    </a>
+                </div>
+                <div class="elemetosMenuLateral" id="11">
+                    <hr>
+                    <a href="anexos.php">
+                        <p class="parrafo1">Anexos <i class="fas fa-arrow-right" ></i></p>
+                    </a>
+                </div>
+                <br>
+            </div>
+        </div>
+
+        <div class="cuadroCate">
+            <div class="titulo_categoria">Anexos</div>
+
+            <div>
+                <div class="parrafo" id="11.1">
+                    <p>
+                        ANEXO 1
+                    </p>
+                </div>
+                <div class="preguntasCategoria" id="SC_11.1.1">
+                    <p>Estudiantes que aprobaron el EGEL-CENEVAL
+                     </p>
+                    <input type="text" id="RA1" placeholder="Escribe tu respuesta aquí...">
+                    <!-- <img src="" alt=""><button>Guardar</button> -->
+                    <br><br>
+                    <div class="Listo">
+                        <img src="/imagenes/pdf.png" alt="">
+                        <button>Seleccionar archivos</button>
+                        <!-- <button>Cargar</button>  -->
+                    </div>
+                    <div class="btnListo"><button>Guardar</button></div>
+                </div>
+
+            <div>
+                <div class="parrafo" id="11.2">
+                    <p>
+                        ANEXO 2
+                    </p>
+                </div>
+                <div class="preguntasCategoria" id="SC_11.2.1">
+                    <p>Estructura Financiera de la Facultad, Escuela, División o Departamento
+                    </p>
+                    <input type="text" id="RA2" placeholder="Escribe tu respuesta aquí...">
+                    <!-- <img src="" alt=""><button>Guardar</button> -->
+                    <br><br>
+                    <div class="Listo">
+                        <img src="/imagenes/pdf.png" alt="">
+                        <button>Seleccionar archivos</button>
+                        <!-- <button>Cargar</button>  -->
+                    </div>
+                    <div class="btnListo"><button>Guardar</button></div>
+            
+            </div>
+
+            <div>
+                <div class="parrafo" id="11.3">
+                    <p>
+                        ANEXO 3
+                    </p>
+                </div>
+                <div class="preguntasCategoria" id="SC_11.3.1">
+                    <p>Nota: Llenar sólo en caso de re-acreditación.                    </p>
+                    <input type="text" id="RA3" placeholder="Escribe tu respuesta aquí...">
+                    <p>Seguimiento de Recomendaciones</p>
+                    <input type="text"  id="RA3-A1" placeholder="Escribe tu respuesta aquí...">
+                    <p>Recomendaciones al programa académico</p>
+                    <input type="text" id="RA3-A2" placeholder="Escribe tu respuesta aquí...">
+                    <p>El anexo 3 relativo a las recomendaciones es para el instrumento que se utilice en la visita de campo.
+                    </p>
+                    <input type="text" id="RA3-A3" placeholder="Escribe tu respuesta aquí...">
+                    <!-- <img src="" alt=""><button>Guardar</button> -->
+                    <br><br>
+                    <div class="Listo">
+                        <img src="/imagenes/pdf.png" alt="">
+                        <button>Seleccionar archivos</button>
+                        <!-- <button>Cargar</button>  -->
+                    </div>
+                    <div class="btnListo"><button>Guardar</button></div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</body>
+
+</html>
