497739ec42190272d93f288110049f05c5888a85
Ahira <tanibugarin@gmail.com>
2023-10-27 12:51:47 -0600

    aaaaaaaaaaaaaaaaaaa

--- categorias/recuperar_respuestasAnexos.php
+++ categorias/recuperar_respuestasAnexos.php
@@ -1,17 +1,13 @@
 <?php
 // Conectar a la base de datos
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "conaic";
+// Conectar a la base de datos
+include "../conexionDB/conexion.php";
+    conecta();
 
-// Crear una conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
+    // Verifica la conexión
+    if ($conexion->connect_error) {
+        die("Error de conexión: " . $conexion->connect_error);
+    }
 
 // Claves a recuperar
 $claves = ["RA1","RA2","RA3","RA3-A1","RA3-A2","RA3-A3"];
@@ -24,11 +20,10 @@ foreach ($claves as $clave) {
     $sql = "SELECT respuesta FROM respuestas WHERE claveRespuesta = '$clave'";
     
     // Ejecutar la consulta
-    $result = $conn->query($sql);
-    
+    $result = $conexion->query($sql);
 
     if ($result === false) {
-        die("Error en la consulta: " . $conn->error);
+        die("Error en la consulta: " . $conexion->error);
     }
 
     // Obtener la respuesta
@@ -49,5 +44,5 @@ foreach ($claves as $clave) {
 echo json_encode($respuestas);
 
 // Cerrar la conexión a la base de datos
-$conn->close();
+$conexion->close();
 ?>
