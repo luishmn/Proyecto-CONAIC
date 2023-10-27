48e995e00d1671e78a680408f6bb6d60260f1139
Ahira <tanibugarin@gmail.com>
2023-10-27 09:19:43 -0600

    aaaaaaaaaaa

--- categorias/guardar_respuesta.php
+++ categorias/guardar_respuesta.php
@@ -6,7 +6,6 @@ $password = "";
 $dbname = "conaic";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
