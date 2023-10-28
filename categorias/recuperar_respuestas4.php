4a9bc2ef5b7a66eb71633409c3e35bbdfcaec98c
oscar23w <63673374+oscar23w@users.noreply.github.com>
2023-10-28 09:15:58 -0600

    categoria4 con respuestas integradas

--- categorias/recuperar_respuestas4.php
+++ categorias/recuperar_respuestas4.php
@@ -1,23 +1,28 @@
 <?php
 // Conectar a la base de datos
 include "../conexionDB/conexion.php";
-    conecta();
+conecta();
 
-    // Verifica la conexión
-    if ($conexion->connect_error) {
-        die("Error de conexión: " . $conexion->connect_error);
-    }
+// Verifica la conexión
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+// Configura la codificación de caracteres en la conexión
+$conexion->set_charset("utf8mb4");
 
 // Claves a recuperar
-$claves = ["RS4-1-1"];
+$claves = ["RS4-1-1", "RS4-1-1A1", "RS4-1-1A2", "R4-1-1A3", "RS4-1-1A4", "R4-1-1A5", "RS4-1-1A6", "R4-1-1A7", "R4-1-1A8", "RS4-1-1A9", "R4-1-1A10", "RS4-1-1A11", "R4-1-1A12", "R4-1-1A13", "RS4-1-1A14", "R4-1-1A15", "RS4-1-1A16", "R4-1-1A17"];
 
 // Inicializar un array para las respuestas
 $respuestas = [];
 
 // Consulta SQL para obtener las respuestas correspondientes a las claves
 foreach ($claves as $clave) {
+    $clave = $conexion->real_escape_string($clave); // Escapar la clave
+
     $sql = "SELECT respuesta FROM respuestas WHERE claveRespuesta = '$clave'";
-    
+
     // Ejecutar la consulta
     $result = $conexion->query($sql);
 
@@ -30,18 +35,17 @@ foreach ($claves as $clave) {
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-        // Convertir la respuesta a minúsculas y eliminar espacios en blanco
-        $respuesta = strtolower(trim($row['respuesta']));
+        // La respuesta ya está en la codificación correcta
+        $respuesta = $row['respuesta'];
     }
 
-    
     // Agregar la respuesta al array de respuestas
-    $respuestas[] = $clave;
+    $respuestas[] = $clave; // Agregar la clave escapada
     $respuestas[] = $respuesta;
 }
 
 // Devolver las respuestas como JSON
-echo json_encode($respuestas);
+echo json_encode($respuestas, JSON_UNESCAPED_UNICODE);
 
 // Cerrar la conexión a la base de datos
 $conexion->close();
