bcd6ee52682c5c8c331c4c9cc704f2d33079406b
Chinonomon <ramongonzalez12312@gmail.com>
2023-10-29 13:22:10 -0600

    .

--- categorias/categoria3.php
+++ categorias/categoria3.php
@@ -1046,7 +1046,7 @@
                 <div id="selected-files1" class="titulosArchs">
                 </div>
                 <br>
-                <button id="botonSubirChido" class="cargar-pdf" data-id="1.1.1">
+                <button id="botonSubirChido" class="cargar-pdf" data-id="3.1.1">
                     <i class="fas fa-upload"></i> Subir PDF
                 </button>
 
--- categorias/categoria5.php
+++ categorias/categoria5.php
@@ -781,7 +781,7 @@
                 <div id="selected-files1" class="titulosArchs">
                 </div>
                 <br>
-                <button id="botonSubirChido" class="cargar-pdf" data-id="2.1.1">
+                <button id="botonSubirChido" class="cargar-pdf" data-id="5.1.1">
                     <i class="fas fa-upload"></i> Subir PDF
                 </button>
 
