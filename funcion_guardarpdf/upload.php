bbed811fb89933511ea4074724646c24240b9b0d
KaronteElPro <alanaceistopherrr1@outlook.com>
2023-10-30 13:32:01 -0600

    funcion pdfs

--- funcion_guardarpdf/upload.php
+++ funcion_guardarpdf/upload.php
@@ -3,7 +3,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $carpeta_destino = "../categorias/pdfs/";
     $id = $_POST["id"];
     $archivos = $_FILES["archivo"];
-
    
     include "../conexionDB/conexion.php";
     conecta();
@@ -11,30 +10,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo "<script>alert('Ocurrió un error al conectar con la Base de Datos. Vuelva a iniciar sesión.');</script>";
     }
 
+    // Consulta SQL para obtener el último número de archivo asociado al criterio
+    $sql = "SELECT MAX(SUBSTRING_INDEX(clavePDF, '-', -1)) AS ultimo_numero FROM SubCriteriosPDF WHERE claveSubCriterio = '$id'";
+    $resultado = $conexion->query($sql);
+    $row = $resultado->fetch_assoc();
+    $ultimo_numero = $row['ultimo_numero'] ?? 0; // Si no hay registros, se asume 0.
+
     foreach ($archivos["error"] as $key => $error) {
         if ($error == UPLOAD_ERR_OK) {
             $nombre_temporal = $archivos["tmp_name"][$key];
             $nombre_archivo = $archivos["name"][$key];
             $extension = pathinfo($nombre_archivo, PATHINFO_EXTENSION);
-            $clavePDF = uniqid("pdf_") . ".pdf";
 
             if ($extension === "pdf") {
-                $nuevo_nombre = $clavePDF; // Utiliza un identificador único para cada archivo
+                $nuevo_numero = $ultimo_numero + 1; // Incrementa el número
+                $nuevo_nombre = "pdf" . $id . "-" . $nuevo_numero . ".pdf"; // Construye el nuevo nombre
                 $ruta_destino = $carpeta_destino . $nuevo_nombre;
 
                 if (move_uploaded_file($nombre_temporal, $ruta_destino)) {
                     // Realiza la inserción en la base de datos
-                    $claveSubCriterio = $id; // Utiliza el valor de $id como claveSubCriterio
-                    //$clavePDF = $nuevo_nombre; // Utiliza el nombre del archivo como clavePDF
-                   
+                    $claveSubCriterio = $id;
 
                     // Consulta SQL para insertar datos en la tabla "subcriteriospdf"
-                    $sql = "INSERT INTO SubCriteriosPDF (claveSubCriterio, clavePDF, nombrePDF) VALUES ('$claveSubCriterio', '$clavePDF', '$nombre_archivo')";
+                    $sql = "INSERT INTO SubCriteriosPDF (claveSubCriterio, clavePDF, nombrePDF) VALUES ('$claveSubCriterio', '$nuevo_nombre', '$nombre_archivo')";
 
                     if ($conexion->query($sql) === TRUE) {
-                        echo "Datos insertados en la tabla subcriteriospdf correctamente para el archivo '$clavePDF'.<br>";
+                        echo "Datos insertados en la tabla subcriteriospdf correctamente para el archivo '$nuevo_nombre'.<br>";
+                        // Actualiza el valor de $ultimo_numero para el próximo archivo
+                        $ultimo_numero = $nuevo_numero;
                     } else {
-                        echo "Error al insertar datos para el archivo '$clavePDF': " . $conexion->error . "<br>";
+                        echo "Error al insertar datos para el archivo '$nuevo_nombre': " . $conexion->error . "<br>";
                     }
                 } else {
                     echo "Error al mover el archivo a la carpeta de destino.<br>";
