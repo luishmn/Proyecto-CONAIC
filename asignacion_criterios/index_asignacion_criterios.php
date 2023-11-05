681b92c9fa77447f9dd44dbf88298659cf62d430
Diego Fregoso <imdiegofregoso@gmail.com>
2023-11-04 21:44:43 -0600

    Revisión de ortografía

--- asignacion_criterios/index_asignacion_criterios.php
+++ asignacion_criterios/index_asignacion_criterios.php
@@ -21,7 +21,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Asiganción de Criterios</title>
+    <title>Asignación de Criterios</title>
     <link rel="stylesheet" href="style.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="style.js"></script>
